3011fec62d04a54b54744ac0cf1fb90a7184a1e7
akaiqt882001234 <anhnpngcd191020@fpt.edu.vn>
2023-03-28 21:45:16 +0700

    Post form

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -17,65 +17,19 @@ export default function AuthModal() {
 
     return (
         <div className="w-screen min-h-screen fixed top-0 left-0 z-20 flex" style={{ backgroundColor: 'rgb(0,0,0,.6)' }}>
-            <div className="bg-reddit_dark border border-reddit_dark-brightest p-5 w-3/4 md:w-1/2 lg:w-1/4 mx-auto rounded-2xl text-reddit_text self-center">
-                {authType === 'login' && (
-                    <>
-                        <h1 className="text-xl mb-5">Login</h1>
+            <form className="bg-reddit_dark border border-reddit_dark-brightest p-5 w-3/4 md:w-1/2 lg:w-1/2 mx-auto rounded-2xl text-reddit_text self-center">
+                        <h1 className="text-xl mb-5">Add New Post</h1>
                         <label>
-                            <span className="text-reddit_text-darker text-sm">Email</span>
-                            <Input type="email" className='mt-1 mb-3 w-full' placeholder="Email" />
+                            <span className="text-reddit_text-darker text-sm">Title</span>
+                            <input type="text" className='mt-1 mb-3 w-full rounded-2xl bg-reddit_dark-brighter border p-2 border-reddit_dark-brightest' placeholder="/Title" />
                         </label>
                         <label>
-                            <span className="text-reddit_text-darker text-sm">Password</span>
-                            <Input type="Password" className='mt-1 mb-3 w-full' placeholder="Password" />
+                            <span className="text-reddit_text-darker text-sm">Content</span>
+                            <textarea type="text" className='mt-1 mb-3 w-full h-40 rounded-2xl bg-reddit_dark-brighter border p-2 border-reddit_dark-brightest resize-none' placeholder="The quick brown fox jumps over the lazy dog" />
                         </label>
-                        <Button className="w-full py-2 mb-3">Login</Button>
-                        <div>
-                            You're a newbie? <button className=" text-orange-500  font-bold" onClick={() => setAuthType('register')}> Register now!!</button>
-                        </div>
-                    </>
-                )}
-                {authType === 'register' && (
-                    <>
-                        <h1 className="text-xl mb-5">Sign up</h1>
-                        <label>
-                            <span className="text-reddit_text-darker text-sm">Email</span>
-                            <Input
-                                type="email"
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                                className='mt-1 mb-3 w-full'
-                                placeholder="Email"
-                            />
-                        </label>
-                        <label>
-                            <span className="text-reddit_text-darker text-sm">Name</span>
-                            <Input
-                                type="text"
-                                value={name}
-                                onChange={e => setName(e.target.value)}
-                                className='mt-1 mb-3 w-full'
-                                placeholder="Kimi no namae wa"
-                            />
-                        </label>
-                        <label>
-                            <span className="text-reddit_text-darker text-sm">Password</span>
-                            <Input
-                                type="Password"
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                                className='mt-1 mb-3 w-full'
-                                placeholder="Password"
-                            />
-                        </label>
-                        <Button className="w-full py-2 mb-3" onClick={e => register(e)}>Sign up</Button>
-                        <div>
-                            Already have an account? <button className=" text-orange-500 font-bold" onClick={() => setAuthType('login')}> Login now!!</button>
-                        </div>
-                    </>
-                )}
-
-            </div>
+                        <Button className="w-full py-2 mb-3">Post</Button>
+                        
+            </form>
         </div>
     )
 }
